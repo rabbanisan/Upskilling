38089366fd8ee42224baec6ebf07985109b44cd4
rabbanisan <bani.ramadhan99@gmail.com>
2022-07-13 09:50:50 +0700

    jenis variable

---
+++ variable.js
@@ -0,0 +1,29 @@
+// == Jenis Variable == 
+// String >> char (a,1,$) '' "" ``
+huruf = 'a,b,c';
+kata = 'satu, dua';
+angka = '1,2,3';
+// Number >> bilangan bulat 2,1 atau bukan 1
+bilangan = 1;
+desimal = 2.5;
+// Boolean >> true & false
+boolean1 = true;
+boolean2 = false;
+
+// Deklarasi Variable
+// var
+var angka = 1;
+var huruf = 'a';
+// let
+let angka = 1;
+let a = 1;
+let b = 1;
+if (a == b) {
+    let b = 2;
+}
+console.log(b);
+// const
+const phi = 3.14;
+const phi = 3;
+
+console.log(phi);
