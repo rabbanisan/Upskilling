c27fab309c24e8e242ad2a6b8ba72d0d4ae255f9
rabbanisan <bani.ramadhan99@gmail.com>
2022-07-13 12:16:09 +0700

    array

---
+++ array.js
@@ -0,0 +1,12 @@
+// Array adalah tipe data yang dapat menampung lebih dari 1 value
+const array = ['satu',1,true,false,NaN,undefined,null];
+//push
+// array.push(2)
+// array[] = 2;
+//pop
+// array.pop();
+//shift
+// array.shift()
+//unshift
+array.unshift(0)
+console.table(array);
